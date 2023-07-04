7855570e7b82aeb1f26fa9735d9e5ab52cfc6a89
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-04 18:38:10 +0200

    Update Virtual assistant and accessing data.py

--- Virtual assistant and accessing data.py
+++ Virtual assistant and accessing data.py
@@ -1,6 +1,10 @@
 import sqlite3
 #connection to the databese, single file
 conn = sqlite3.connect('hotels.db')
+#in order to actually run queries, we need to create a cursor using this connection
+c = conn.cursor()
+#by calling connection dot cursor we can execute queries by passing a string c.execute() method
+c.execute( "SELECT * FROM hotels WHERE area =  'south' and pricerange = 'hi' ")
 
 
-SELECT name from restaurants WHERE area = 'center' AND pricerange = 'hi'
+SELECT name from restaurants WHERE area = 'south' AND pricerange = 'hi'

7ef58280679e383063298cb0bfa2b6a3ab0c26ec
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-04 18:41:53 +0200

    Update Virtual assistant and accessing data.py

--- Virtual assistant and accessing data.py
+++ Virtual assistant and accessing data.py
@@ -5,6 +5,8 @@ conn = sqlite3.connect('hotels.db')
 c = conn.cursor()
 #by calling connection dot cursor we can execute queries by passing a string c.execute() method
 c.execute( "SELECT * FROM hotels WHERE area =  'south' and pricerange = 'hi' ")
+#after that, we can get the results from the cursor object, using the cursor's fetchall method, this returns a list of tuples.
+c.fetchall()
 
-
-SELECT name from restaurants WHERE area = 'south' AND pricerange = 'hi'
+#result
+[( 'Grand Hotel', 'hi', 'south', 5)]

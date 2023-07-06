a079d3c8e7f3eb5cd572ce8584c2c7a66a588711
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-06 11:15:04 +0200

    Update Virtual assistant and accessing data.py

--- Virtual assistant and accessing data.py
+++ Virtual assistant and accessing data.py
@@ -17,3 +17,23 @@ c = conn.cursor()
 c.execute (Select name from hotels where price = 'mid' AND area = 'north')
 c.fetchall
 
+
+#exercise2
+# Import sqlite3
+import sqlite3
+
+# Open connection to DB
+conn = sqlite3.connect('hotels.db')
+
+# Create a cursor
+c = conn.cursor()
+
+# Define area and price
+area, price = "south", "hi"
+t = (area, price)
+
+# Execute the query
+c.execute("SELECT * FROM hotels WHERE area= 'south' AND price= 'hi' ")
+
+# Print the results
+print(c.fetchall())

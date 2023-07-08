b981a63492e26bc35356bc1e3869a56eb8e68ea3
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-08 09:29:18 +0200

    Update General chatbotstips.py

--- General chatbotstips.py
+++ General chatbotstips.py
@@ -1,1 +1,5 @@
+# If you want to create delay in the chatbot response you need to import the time framework and used the timesleep method.
+import time
+time.sleep(0.5)
+# the argument of the method is the amount of seconds of the delay
 

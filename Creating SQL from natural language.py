6922e04d51f9f83bc30bd41fb4954af416d2b20e
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-11 11:14:13 +0200

    Update Creating SQL from natural language.py

--- Creating SQL from natural language.py
+++ Creating SQL from natural language.py
@@ -1,1 +1,17 @@
+# Define respond()
+def respond(message):
+    # Extract the entities
+    entities = ____.____(____)["____"]
+    # Initialize an empty params dictionary
+    params = {}
+    # Fill the dictionary with entities
+    for ent in entities:
+        params[ent["entity"]] = str(ent["value"])
 
+    # Find hotels that match the dictionary
+    results = ____
+    # Get the names of the hotels and index of the response
+    names = [r[0] for r in results]
+    n = ____(len(results),3)
+    # Select the nth element of the responses array
+    return ____[n].____(*names)

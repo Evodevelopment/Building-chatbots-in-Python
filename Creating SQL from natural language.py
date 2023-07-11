0ee06e6d067758d0b9297ef90ecfc5a777b7b46a
JDdevelopment <66202511+Evodevelopment@users.noreply.github.com>
2023-07-11 11:20:04 +0200

    Update Creating SQL from natural language.py

--- Creating SQL from natural language.py
+++ Creating SQL from natural language.py
@@ -18,4 +18,20 @@ def respond(message):
 
 print(respond("I want an expensive hotel in the south of town"))
 
+#In this code, it is assumed that you have an interpreter object that provides the .parse() method to extract the entities from the 
+#message. The entities are extracted using interpreter.parse(message)["entities"].
+
+#The code then initializes an empty params dictionary and fills it with the extracted entities, with each entity's name as the key 
+#and its value as the value in the dictionary.
+
+#Next, the find_hotels() function is called with the params dictionary to obtain the matching hotel results. 
+#The code retrieves the names of the hotels from the results and assigns them to the names list.
+
+#The variable n is determined using the min() function to choose the index for the response. 
+#It takes the minimum value between the number of results and 3.
+
+#Finally, the code returns the selected response from the responses list using the .format() method to insert the names of the hotels 
+#into the response.
+
+#Please ensure that you have implemented the find_hotels() function and have the responses list defined appropriately.
 
